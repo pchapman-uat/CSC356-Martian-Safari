3da9dd8a6faa4abaa5f19ed93b8ae9aec7f7e854
Preston Chapman <pchapman82070@uat.edu>
2024-09-17 13:15:08 -0700

    Adjust HTML

--- main.php
+++ main.php
@@ -9,21 +9,24 @@
 <body>
     <h1>Insert Company Name Here</h1>
     <main>
-        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Vestibulum eget neque bibendum, eleifend lectus non, 
-            luctus ligula. Etiam lobortis orci velit, at pulvinar 
-            turpis laoreet sed. Duis vel ante lectus. 
-            Mauris eget ornare felis. Nam sed ipsum ac nisi 
-            tristique vestibulum at sollicitudin leo. Morbi 
-            vestibulum eros ut leo placerat mollis. Nam rutrum, 
-            justo eu sodales placerat, ante sem auctor dolor, 
-            et ullamcorper elit dolor vitae purus. 
-            Aenean augue nunc, egestas posuere consequat id,
-             auctor vitae nibh. Sed a sem fermentum, 
-             molestie nibh vel, volutpat felis. 
-             Morbi maximus commodo est, nec sagittis mi placerat at. 
-             Praesent at fringilla arcu. Orci varius natoque penatibus et magnis dis parturient 
-             montes, nascetur ridiculus mus. In ut ornare arcu.</p>
+        <section class="description">
+            <h2>About Our Company</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Vestibulum eget neque bibendum, eleifend lectus non, 
+                luctus ligula. Etiam lobortis orci velit, at pulvinar 
+                turpis laoreet sed. Duis vel ante lectus. 
+                Mauris eget ornare felis. Nam sed ipsum ac nisi 
+                tristique vestibulum at sollicitudin leo. Morbi 
+                vestibulum eros ut leo placerat mollis. Nam rutrum, 
+                justo eu sodales placerat, ante sem auctor dolor, 
+                et ullamcorper elit dolor vitae purus. 
+                Aenean augue nunc, egestas posuere consequat id,
+                auctor vitae nibh. Sed a sem fermentum, 
+                molestie nibh vel, volutpat felis. 
+                Morbi maximus commodo est, nec sagittis mi placerat at. 
+                Praesent at fringilla arcu. Orci varius natoque penatibus et magnis dis parturient 
+                montes, nascetur ridiculus mus. In ut ornare arcu.</p>
+            </section>
     </main>
     <script src="script/main.js"></script>
 </body>
