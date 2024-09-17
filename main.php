f6fc054e661694baeed10c23e9d5b7df4b8c9c05
Preston Chapman <pchapman82070@uat.edu>
2024-09-17 12:55:01 -0700

    Begin PHP

---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSC356 2.2</title>
+</head>
+<body>
+    <h1>Insert Company Name Here</h1>
+    <main>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Vestibulum eget neque bibendum, eleifend lectus non, 
+            luctus ligula. Etiam lobortis orci velit, at pulvinar 
+            turpis laoreet sed. Duis vel ante lectus. 
+            Mauris eget ornare felis. Nam sed ipsum ac nisi 
+            tristique vestibulum at sollicitudin leo. Morbi 
+            vestibulum eros ut leo placerat mollis. Nam rutrum, 
+            justo eu sodales placerat, ante sem auctor dolor, 
+            et ullamcorper elit dolor vitae purus. 
+            Aenean augue nunc, egestas posuere consequat id,
+             auctor vitae nibh. Sed a sem fermentum, 
+             molestie nibh vel, volutpat felis. 
+             Morbi maximus commodo est, nec sagittis mi placerat at. 
+             Praesent at fringilla arcu. Orci varius natoque penatibus et magnis dis parturient 
+             montes, nascetur ridiculus mus. In ut ornare arcu.</p>
+    </main>
+</body>
+</html>
