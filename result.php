c2e73933c05b37029230017e6234df8e4e47352e
Preston Chapman <pchapman82070@uat.edu>
2024-09-26 12:34:25 -0700

    Add result php

---
+++ result.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/main.css">
+</head>
+<body>
+
+    <h1>Martian Safari</h1>
+    <div class="logoDiv">
+        <img src="assets/placeholder-logo.png">
+    </div>
+
+    <main>
+        <section class="description">
+            <h2>Thank you!</h2>
+            <p>Your fourm request has been recived and will be processed shortly</p>
+        </section>
+    </main>
+
+</body>
+</html>
