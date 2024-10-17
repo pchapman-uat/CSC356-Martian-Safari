08f10086efe34901f126ab930c7bccb46343690e
Preston Chapman <pchapman82070@uat.edu>
2024-10-17 12:53:09 -0700

    add comments

--- result.php
+++ result.php
@@ -1,59 +1,93 @@
 <?php
     // Display all post keys 
     function getKeys(): void{
+        // Convert post keys to array
         $post_keys = array_keys($_POST);
 
+        // For each loop, this saves each value from array as a new varriable
         foreach ($post_keys as $key) {
             echo $key . "<br>";
         }
     }
 
+    // Check the age returning a boolean if the age is valid
+    // Check for non number 
     function checkAge(): bool {
+        // Get age value from post
         $age = $_POST["age"];
+        // Try to convert to integer
         try {
             $age = (int) $age;
         } catch (TypeError) {
             return false;
         }
+        // Return if the age is 18 or more
         return $age >= 18;
     }
 
+
+    // Get the chance based on the age value
+    // Returns a float from 0-1
     function getAgeChance(): float{
+        // Check if the age is valid
         if(checkAge()){
+            // Since the age is valid it must be a number
             $age = (int) $_POST["age"];
 
+            // Find the difference from the target age
             $result = 30 - $age;
+            // Get the absolute value of the difference
             $result = abs($result);
 
+            // If the result is greater than 30, such as an age of 0, or 60, default to 0
             if($result >= 30){
                 return 0.0;
             }else {
+                // Return a percentage difference based on the target value
                 return  (30-$result) / 30;
             }
         } else {
             return 0;
         }
     }
+
+    // Get the experince value
+    // Returns a float from 0-1
     function getExperince():float {
         return (float) $_POST["experince"] / 5;
     }
+    // Calcualate the application chance
+    // Returns a float from 0-1
+
     function applicationChance(): float{
+        // Total number of checks
         $totalChecks = 2;
+        // Current value from the checks
         $currChecks = 0;
+        // If the age is not valid return 0
         if(!checkAge()) return 0;
+        // Add the age chance to the total
         $currChecks += getAgeChance();
+        // Add the experince chance to the total
         $currChecks += getExperince();
 
+        // Return a float percentage for the chance
         return $currChecks / $totalChecks;
     }
 
+    // Convert a number to a percentage string
     function percentFormat($num): string{
         return $num*100 . "%";
     }
 
+    // Main function that calculates and displays the chance
+    // Returns the adjusted percetage string
     function calcChance(): string{
+        // Get the application chance
         $num = applicationChance();
+        // If the number is 1 or higher, set it to 99
         if($num >= 1) $num = 0.99;
+        // Get the percent format version
         $result = percentFormat($num);
         return $result;
     }
