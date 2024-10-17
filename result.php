a0eb4db88bdc180c0bcae2866652ef819dbcc454
Preston Chapman <pchapman82070@uat.edu>
2024-10-17 12:38:34 -0700

    Create calculations based on user age and expereince

--- result.php
+++ result.php
@@ -1,6 +1,6 @@
 <?php
     // Display all post keys 
-    function getKeys(){
+    function getKeys(): void{
         $post_keys = array_keys($_POST);
 
         foreach ($post_keys as $key) {
@@ -16,6 +16,46 @@
             return false;
         }
         return $age >= 18;
+    }
+
+    function getAgeChance(): float{
+        if(checkAge()){
+            $age = (int) $_POST["age"];
+
+            $result = 30 - $age;
+            $result = abs($result);
+
+            if($result >= 30){
+                return 0.0;
+            }else {
+                return  (30-$result) / 30;
+            }
+        } else {
+            return 0;
+        }
+    }
+    function getExperince():float {
+        return (float) $_POST["experince"] / 5;
+    }
+    function applicationChance(): float{
+        $totalChecks = 2;
+        $currChecks = 0;
+        if(!checkAge()) return 0;
+        $currChecks += getAgeChance();
+        $currChecks += getExperince();
+
+        return $currChecks / $totalChecks;
+    }
+
+    function percentFormat($num): string{
+        return $num*100 . "%";
+    }
+
+    function calcChance(): string{
+        $num = applicationChance();
+        if($num >= 1) $num = 0.99;
+        $result = percentFormat($num);
+        return $result;
     }
 
 ?>
@@ -37,6 +77,7 @@
     <main>
         <section class="description">
             <h2> Welcome <?php echo $_POST["fName"] . " " . $_POST["lName"]; ?>!</h2>
+            <h2>Your chance of getting accepted is: <?php echo calcChance()?></h2>
             <p>Your fourm request has been recived and will be processed shortly</p>
         </section>
     </main>
