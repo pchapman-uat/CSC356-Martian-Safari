a5729191601361113efd9ee73e318ec57b8199b1
Preston Chapman <pchapman82070@uat.edu>
2024-10-29 12:45:16 -0700

    Add Form

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/login.css">
 </head>
 <body>
     <h1>Martian Safari</h1>
@@ -13,8 +14,19 @@
     </div>
     <section class="application">
         <h2>Login</h2>
-    <form>
-        
+    <form class="form">
+        <span>
+            <label for="uerID">User ID</label>
+            <input id="userID" name="userID" type="text" required>
+        </span>
+        <span>
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" required>
+        </span>
+        <span>
+            <button type="submit">Submit</button>
+        </span>
+       
     </form>
     </section>
 </body>
