1fe4a41a5dce20e6d2e8b0a205cf4bc6c5a83b1e
Preston Chapman <pchapman82070@uat.edu>
2024-10-29 12:48:40 -0700

    Begin Login Validation

---
+++ script/login.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", formSubmit)
+
+/**
+ * @param {SubmitEvent} event 
+ */
+async function formSubmit(event) {
+    console.log("Form Subbited")
+    event.preventDefault();
+}
