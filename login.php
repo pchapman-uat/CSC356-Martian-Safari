349b07c81a391afdfde76ef784a90bf18cefdb96
Preston Chapman <pchapman82070@uat.edu>
2024-10-29 12:41:41 -0700

    Begin Login

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+    <h1>Martian Safari</h1>
+    <div class="logoDiv">
+        <img src="assets/placeholder-logo.png">
+    </div>
+    <section class="application">
+        <h2>Login</h2>
+    <form>
+        
+    </form>
+    </section>
+</body>
+</html>
