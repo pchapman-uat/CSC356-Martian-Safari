0d0f398c8ae0b00b43e23b11f606d64a6a739bf8
Preston Chapman <pchapman82070@uat.edu>
2024-10-31 12:36:54 -0700

    Adjust form logic

--- script/login.js
+++ script/login.js
@@ -1,9 +1,39 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", formSubmit)
-
 /**
  * @param {SubmitEvent} event 
  */
 async function formSubmit(event) {
-    console.log("Form Subbited")
+    event.preventDefault();
+
+    const username = form.querySelector("#username");
+    const password = form.querySelector("#password");
+
+    const reasons = validateForm(username, password);
+    const result = document.getElementById("result")
+    if(reasons.length > 0){
+        for(let i = 0; reasons.length; i++){
+            let element = document.createElement("p");
+            element.textContent = reasons[i];
+            result.appendChild(element);
+        }
+    }
+}
+
+/**
+ * 
+ * @param {String} username 
+ * @param {String} password 
+ */
+function validateForm(username, password){
+    console.log(username)
+    var reasons = [];
+    if(username == ""){
+        reasons.push("Username can not be blank")
+    }
+    if(password == ""){
+        reasons.push("Password can not be blank")
+    }else if(password.length < 8){
+        reasons.push("Password must be more than 8 characters")
+    }
+
+    return reasons;
 }
