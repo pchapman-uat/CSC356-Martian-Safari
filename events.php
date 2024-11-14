bed6e4f228f243f12e399ba7be4961045840a7ce
Preston Chapman <pchapman82070@uat.edu>
2024-11-14 12:57:12 -0700

    Adjust Event Format

--- events.php
+++ events.php
@@ -37,9 +37,15 @@
 
         <section class="events">
             <div class="event">
-                <h4>Example Title</h4>
-                <h5>Example Subtitle</h5>
-                <img src="https://placehold.co/600x400">
+                <div class="content">
+                    <h4>Example Title</h4>
+                    <h5>Example Subtitle</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+
+                <div class="imgDiv">
+                     <img src="https://placehold.co/600x400">
+                </div>
             </div>
         </section>
     </main>
