a930923ef344f893a3a1757e948d756221459ec3
Preston Chapman <pchapman82070@uat.edu>
2024-11-19 12:54:03 -0700

    Events connection with Database

--- events.php
+++ events.php
@@ -11,7 +11,38 @@
         }
     }
 
+    function getConfig(){
+        $str = file_get_contents(__DIR__ . '/config/config.json');
+        return json_decode($str, true);
+    }
+
+    function getEvents(){
+        $data = getConfig();
+        $dbConn = mysqli_connect($data["hostname"], $data["username"], $data["password"], $data["databaseName"]);
+
+        if(!$dbConn){
+            echo "Connection Failed";
+            return;
+        }
+
+        $sql = "SELECT * FROM Tours";
+
+        $result = mysqli_query($dbConn, $sql);
+        
+
+        $len = mysqli_num_rows($result);
+
+        for($i = 0; $i < $len; $i++){
+            $data = mysqli_fetch_array($result);
+            makeEvent($data["name"], $data["date"], "placeholder");
+        }
+    }
+
     checkSession();
+
+    function makeEvent($header, $subheader, $text){
+        include "static/event.php";
+    }
 ?>
 
 
@@ -36,17 +67,7 @@
         </section>
 
         <section class="events">
-            <div class="event">
-                <div class="content">
-                    <h4>Example Title</h4>
-                    <h5>Example Subtitle</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-
-                <div class="imgDiv">
-                     <img src="https://placehold.co/600x400">
-                </div>
-            </div>
+            <?php getEvents()?>
         </section>
     </main>
 
---
+++ static/event.php
@@ -0,0 +1,11 @@
+<div class="event">
+        <div class="content">
+            <h4><?php echo $header?></h4>
+            <h5><?php echo $subheader?></h5>
+            <p><?php echo $text?></p>
+        </div>
+
+        <div class="imgDiv">
+                <img src="https://placehold.co/600x400">
+        </div>
+</div>
