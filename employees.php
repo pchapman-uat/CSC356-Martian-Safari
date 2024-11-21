ab1a975250f12daebfb0b1c76313c0b1f604545c
Preston Chapman <pchapman82070@uat.edu>
2024-11-21 12:52:53 -0700

    Add employees

---
+++ employees.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/main.css">
+    <link rel="stylesheet" href="./style/event.css">
+</head>
+<body>
+    <?php include "static/header.html"?>
+
+    <main>
+    <section class="description">
+            <h3>This is information about our employees!</h3>
+            <p>Currently we are not done with this section so be sure to check back later!</p>
+        </section>
+    </main>
+</body>
+</html>
