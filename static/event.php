dbff407e92d2968d17ed47a3e0549c8759723a37
Preston Chapman <pchapman82070@uat.edu>
2024-11-30 13:44:55 -0700

    Adjust Event Formating

--- static/event.php
+++ static/event.php
@@ -1,11 +1,11 @@
 <div class="event">
-        <div class="content">
-            <h4><?php echo $header?></h4>
-            <h5><?php echo $subheader?></h5>
-            <p><?php echo $text?></p>
-        </div>
+    <div class="content">
+        <h4><?php echo $header ?></h4>
+        <h5><?php echo $subheader ?></h5>
+        <p><?php echo $text ?></p>
+    </div>
 
-        <div class="imgDiv">
-                <img src=<?php echo $url?>>
-        </div>
+    <div class="imgDiv">
+        <img src="<?php echo $url ?>">
+    </div>
 </div>
