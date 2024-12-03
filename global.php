67a17407e26e5ab62d9a295c1cc133a92d42119e
Preston Chapman <pchapman82070@uat.edu>
2024-12-03 13:18:23 -0700

    Move database connection to global

--- global.php
+++ global.php
@@ -13,6 +13,7 @@
         public $password = "";
         public $port = 0;
         public $databaseName = "";
+        public $debug = false;
 
         public function __construct($var) {
             $this->username = $var["username"];
@@ -20,5 +21,48 @@
             $this->password = $var["password"];
             $this->port = $var["port"];
             $this->databaseName = $var["databaseName"];
+            $this->debug = $var["debug"];
         }
+    }
+
+
+    function getData(String $table){
+
+        $data = getConfig();
+        if($data->debug == true){
+            exampleElement();
+            return;
+        }
+
+        // Connect to the database using the config data
+        $dbConn = mysqli_connect($data->hostname, $data->username, $data->password, $data->databaseName);
+        // If  the connection fails, display an error message and return
+        if(!$dbConn){
+            echo "Connection Failed";
+            return;
+        }
+
+        // Select all data from the employees table
+        // NOTE: This should be changed to a specific query
+        $sql = "SELECT * FROM `$table`";
+
+        $result = mysqli_query($dbConn, $sql);
+        
+        return $result;
+    }
+
+    // Create an employee, URL defaults to a placeholder image
+    function makeElement($header, $subheader, $text, $url = "https://placehold.co/600x400"){
+        // Include the event template page, parameters are automatically passed
+        include "static/event.php";
+    }
+
+    // Create example employees
+    function exampleElement(){
+        // Create 5 employees
+        makeElement("Header 1","subHeader 1" , "");
+        makeElement("Header 2", "subHeader 2", "");
+        makeElement("Header 3", "subHeader 3", "");
+        makeElement("Header 4", "subHeader 4", "");
+        makeElement("Header 5", "subHeader 5", "");
     }
